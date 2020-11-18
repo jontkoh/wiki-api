406b7e70f4f96ffdc51d595769250fcadf44358b
Jonathan Koh <jontkoh@gmail.com>
2020-11-18 12:23:55 -0800

    added initial code to / for testing db

--- app.js
+++ app.js
@@ -3,12 +3,30 @@ const mongoose = require("mongoose");
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/wikiAppDB",
-                 {useNewUrlParser: true, useUnifiedTopology: true});
+                {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Article = new mongoose.model("Article", articleSchema)
 
 app.get('/', (req, res) => {
-  res.send("hello world");
-})
+  const article = new Article({
+    title: "title test", 
+    content: "content test"
+  });
+  article.save((err) => {
+    if (err) {
+      console.log(err);
+    }
+    else{
+      res.send("saved the article");
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log("running on port 3000");
-})
+});
